9e3e815096605514684271978c60dc436067180d
dandruf <54970918+dandruf@users.noreply.github.com>
2020-07-31 20:09:23 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Jasmyne Roberts on 7/31/20.
+//  Copyright © 2020 Jasmyne Roberts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
