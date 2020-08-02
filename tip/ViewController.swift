a732b1166d8cc3689e2423019db4417a1c784bf2
dandruf <54970918+dandruf@users.noreply.github.com>
2020-08-02 13:38:32 -0400

    implemented app functionalities

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tipAmountLabel.text = String(format: "$%.2f", 0)
+        totalLabel.text = String(format: "$%.2f", 0)
     }
 
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        // make the keypad go away when you tap off
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // get bill amount
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        // calcuate tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // update the tip and total labels
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
